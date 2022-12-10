bbf26e783759cdbc93fde7707339ebdb635bcee5
darshan45672 <drshnbhandary@gmail.com>
2022-12-10 07:49:22 +0530

    new file

--- MatchStickGamw.c
+++ MatchStickGamw.c
@@ -1,1 +1,36 @@
 /* stay tuned guys */
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(void) {
+int m = 21, p, c;
+printf("\n\t\tRules: you pick 1 or 2 or 3 or 4 matchsticks at a time!!!\n\n\n\n");
+
+while (1) {
+    printf("\nNumber of Match sticks left = %d\n", m);
+    printf("Pick 1 or 2 or 3 or 4 matches\n");
+    scanf("%d", &p);
+
+    if (p > 4 || p < 1){
+        printf("follow the rules\n");
+        continue;
+    }
+    m = m - p;
+
+    printf("Number of matches left = %d\n", m);
+
+    c = 5 - p;
+
+    printf("out of which computer picked up %d\n", c);
+
+    m = m - c;
+
+    if (m == 1) {
+      printf("\nNumber of matches left = %d\n", m);
+      printf("You lost the Game\n");
+      break;
+    }
+  }
+
+  return 0;
+}
